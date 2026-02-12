9e05dc8c511386f24bfe2a64e1bc2082e8929854
Duke Norsworthy <duke.norsworthy@gmail.com>
2026-02-12 11:02:26 -0500

    login and auth dtos

---
+++ api/DTOs/Auth/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+namespace api.Dto.Auth
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public int UserId { get; set; }
+    }
+}
